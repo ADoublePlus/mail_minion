5149234cf4bb1f5330882d1cf975c0d3e2bd4586
John Wonson <jmpwonson@gmail.com>
2021-01-12 15:51:52 +1100

    Create unit_test.php

---
+++ unit_test.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once("minion.class.php");
+
+$parms = array(
+    "host" => "localhost",
+    "port" => 993,
+    "user" => "username",
+    "pass" => "password",
+    "mailType" => MailType::IMAP,
+    "ssl" => TRUE,
+    "validate" => FALSE
+);
+
+$mm = new MailMinion($parms);
+$mm->getFirst(SelectType::UNSEEN);
+
+if ($mm->getError() == MinionError::NO_ERROR)
+{
+    if (!$mm->atEnd())
+    {
+        echo "Message Count: " . $mm->getMailCount() . "<br>\r";
+
+        while (!$mm->atEnd())
+        {
+            $mm->dump();
+            $mm->getNext();
+        }
+    }
+    else
+    {
+        echo "No mail to process<br>\r";
+    }
+}
+else
+{
+    echo "Error opening mailbox<br>\r";
+}
+
+?>
